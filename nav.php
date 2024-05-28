4b4eef90242b94a4ef32d02b51f7244e848db2f1
LUKA\lukas <lukasudarevic03@gmail.com>
2024-05-28 14:54:48 +0200

    nav

--- nav.php
+++ nav.php
@@ -18,7 +18,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active text-white" aria-current="page" href="index.php">Početna</a>
+                    <a class="nav-link active text-white" aria-current="page" href="/Receptar/index.php">Početna</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link text-white" href="login/RegistrationMode.php">Pretraga</a>
