6afebbe95ad0a12e08790cc198340e7ffe2cbbc0
LUKA\lukas <lukasudarevic03@gmail.com>
2024-05-28 13:35:55 +0200

    no message

--- login/Login&Registration.php
+++ login/Login&Registration.php
@@ -9,7 +9,7 @@ include('nav.php'); // Replace 'nav.php' with the correct path if it's in a diff
     <title>Recept Aplikacija - Demo</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <script src="JS/scypt.js"></script>
+    <script src="../JS/scypt.js"></script>
 
 </head>
 <body>
